f3fafa917ee7aeaf3e34bd24bcdc324d567ebcc5
T-megala <megala03.2002@gmail.com>
2024-04-04 22:56:02 +0530

    initialized

---
+++ linq practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+
+class Num
+{
+    static void Main(string[] args)
+    {
+        int[] num = { 9, 3, 4, 5, 6, 7, 8, 14, 10, 11, 12, 1, 2, 20 };
+
+        var rank = from i in num where i<10 orderby i ascending select i;
+
+        foreach(var i in rank ) 
+        { 
+            Console.WriteLine(i);
+        }
+    }
+}
+
